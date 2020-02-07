8959153c7cb19c14b647e37974d5c5cc684c8f89
Keithkiarie <keithkiarie@gmail.com>
2020-02-07 13:50:15 +0300

    removed unnecessary libraries

--- crypto.cs
+++ crypto.cs
@@ -1,13 +1,9 @@
 using System;
 using System.IO;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Linq;
 
 
 class Program
